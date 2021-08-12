d002e10966ee5acceb09f5886c7f68c1b6ef5823
Oleksandr Shtromberg <onestromberg@gmail.com>
2021-08-12 20:27:39 +0300

    added forge_2d

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'blob_sample.dart';
+import 'package:flame/game.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,17 +13,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
+          backgroundColor: Colors.yellow),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -67,47 +69,54 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    return GameWidget(game: BlobSample());
+    // Scaffold(
+    //   appBar: AppBar(
+    //     // Here we take the value from the MyHomePage object that was created by
+    //     // the App.build method, and use it to set our appbar title.
+    //     title: Text(widget.title),
+    //   ),
+    //   body: Center(
+    //     // Center is a layout widget. It takes a single child and positions it
+    //     // in the middle of the parent.
+    //     child: Column(
+    // Column is also a layout widget. It takes a list of children and
+    // arranges them vertically. By default, it sizes itself to fit its
+    // children horizontally, and tries to be as tall as its parent.
+    //
+    // Invoke "debug painting" (press "p" in the console, choose the
+    // "Toggle Debug Paint" action from the Flutter Inspector in Android
+    // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+    // // to see the wireframe for each widget.
+    // //
+    // // Column has various properties to control how it sizes itself and
+    // // how it positions its children. Here we use mainAxisAlignment to
+    // // center the children vertically; the main axis here is the vertical
+    // // axis because Columns are vertical (the cross axis would be
+    // // horizontal).
+    // mainAxisAlignment: MainAxisAlignment.center,
+    // children: <Widget>[
+    //   // Text(
+    //   //   'You have pushed the button this many times:',
+    //   // ),
+    //   // Text(
+    //   //   '$_counter',
+    //   //   style: Theme.of(context).textTheme.headline4,
+    //   // ),
+    //         Container(
+    //           width: 300,
+    //           height: 300,
+    //           child: GameWidget(game: BlobSample()),
+    //           color: Color.fromRGBO(255, 255, 255, 255),
+    //         )
+    //       ],
+    //     ),
+    //   ),
+    //   floatingActionButton: FloatingActionButton(
+    //     onPressed: _incrementCounter,
+    //     tooltip: 'Increment',
+    //     child: Icon(Icons.add),
+    //   ), // This trailing comma makes auto-formatting nicer for build methods.
+    // );
   }
 }
